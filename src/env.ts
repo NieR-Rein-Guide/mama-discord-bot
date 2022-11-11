4a6e76abd450efb2316e167646984cf1b8fbec7b
KeziahMoselle <apps@keziahmoselle.fr>
2022-11-12 00:36:31 +0100

    chore: fix which env is required

--- src/env.ts
+++ src/env.ts
@@ -5,9 +5,13 @@ export const env = envsafe({
     devDefault: 'development',
     choices: ['development', 'production'],
   }),
-  DISCORD_CLIENT_ID: str({}),
   DISCORD_BOT_TOKEN: str({}),
+  DISCORD_CLIENT_ID: str({
+    allowEmpty: true,
+    desc: 'Used to refresh slash commands in dev mode.',
+  }),
   DISCORD_GUILD_ID: str({
+    allowEmpty: true,
     desc: 'Used to refresh slash commands in dev mode.',
   }),
   API_URL: url({
