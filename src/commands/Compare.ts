ee17aa509052c6d4ca4a11154bb9d53395d520b4
Nemesis <contact@keziahmoselle.fr>
2023-01-04 22:45:19 +0100

    feat(compare): compare weapons command

--- src/commands/Compare.ts
+++ src/commands/Compare.ts
@@ -69,7 +69,7 @@ export default class Costume implements BaseDiscordCommand {
     }
   }
 
-  getItem(value, type: string) {
+  getItem(value, type: 'weapons' | 'costumes') {
     if (type === 'costumes') {
       let costume = this.costumes.find((costume) => `${costume.costume_id}` === value)
 
@@ -99,7 +99,7 @@ export default class Costume implements BaseDiscordCommand {
       weapon = firstResult.item as ApiWeapon
     }
 
-    return
+    return weapon
   }
 
   getDifferenceEmoji(value: number) {
@@ -126,7 +126,7 @@ export default class Costume implements BaseDiscordCommand {
     return ':snail:'
   }
 
-  async getItemDescriptionCostume(firstCostume: ApiCostume, secondCostume: ApiCostume) {
+  getItemDescriptionCostume(firstCostume: ApiCostume, secondCostume: ApiCostume) {
     const differences = {
       hp: firstCostume.costume_stat[0].hp -  secondCostume.costume_stat[0].hp,
       atk: firstCostume.costume_stat[0].atk -  secondCostume.costume_stat[0].atk,
@@ -152,8 +152,33 @@ export default class Costume implements BaseDiscordCommand {
     return description
   }
 
+  getItemDescriptionWeapon(firstWeapon: ApiWeapon, secondWeapon: ApiWeapon) {
+    const differences = {
+      hp: firstWeapon.weapon_stat[0].hp -  secondWeapon.weapon_stat[0].hp,
+      atk: firstWeapon.weapon_stat[0].atk -  secondWeapon.weapon_stat[0].atk,
+      vit: firstWeapon.weapon_stat[0].vit -  secondWeapon.weapon_stat[0].vit,
+      cooldown1: firstWeapon.weapon_skill_link[0].weapon_skill.cooldown_time / 30 - secondWeapon.weapon_skill_link[0].weapon_skill.cooldown_time / 30,
+      cooldown2: firstWeapon.weapon_skill_link[1].weapon_skill.cooldown_time / 30 - secondWeapon.weapon_skill_link[1].weapon_skill.cooldown_time / 30
+    }
+
+    let description = ``
+
+    /** STATS */
+    description += `
+      ${emojis.hp} \`${firstWeapon.weapon_stat[0].hp.toString().padEnd(7)}            ${secondWeapon.weapon_stat[0].hp}\` ${emojis.hp.toString().padStart(7)} ${this.getDifferenceEmoji(differences.hp)} ${differences.hp}
+      ${emojis.atk} \`${firstWeapon.weapon_stat[0].atk.toString().padEnd(6)}            ${secondWeapon.weapon_stat[0].atk.toString().padStart(6)}\` ${emojis.atk} ${this.getDifferenceEmoji(differences.atk)} ${differences.atk}
+      ${emojis.def} \`${firstWeapon.weapon_stat[0].vit.toString().padEnd(6)}            ${secondWeapon.weapon_stat[0].vit.toString().padStart(6)}\` ${emojis.def} ${this.getDifferenceEmoji(differences.vit)} ${differences.vit}
+    `.trim()
+
+    /** SKILLS */
+    description += `\n${emojis.skill} \`CD ${firstWeapon.weapon_skill_link[0].weapon_skill.cooldown_time / 30}sec        ${secondWeapon.weapon_skill_link[0].weapon_skill.cooldown_time / 30}sec CD\` ${emojis.skill}  ${this.getSkillDifferenceEmoji(differences.cooldown1)} ${differences.cooldown1}`
+    description += `\n${emojis.skill} \`CD ${firstWeapon.weapon_skill_link[1].weapon_skill.cooldown_time / 30}sec        ${secondWeapon.weapon_skill_link[1].weapon_skill.cooldown_time / 30}sec CD\` ${emojis.skill}  ${this.getSkillDifferenceEmoji(differences.cooldown2)} ${differences.cooldown2}`
+
+    return description
+  }
+
   async run (interaction: ChatInputCommandInteraction): Promise<void> {
-    const selectedType = interaction.options.getString('type')
+    const selectedType = interaction.options.getString('type') as 'weapons' | 'costumes'
     const firstItem = this.getItem(interaction.options.getString('first-item'), selectedType)
     const secondItem = this.getItem(interaction.options.getString('second-item'), selectedType)
 
@@ -171,16 +196,21 @@ export default class Costume implements BaseDiscordCommand {
     }
 
     if (selectedType === 'costumes') {
-      const firstCharacterSlug = urlSlug(firstItem.character.name)
-      const secondCharacterSlug = urlSlug(secondItem.character.name)
+      // @ts-expect-error aa
+      const firstCostume: ApiCostume = firstItem as unknown
+      // @ts-expect-error aa
+      const secondCostume: ApiCostume  = secondItem as unknown
+
+      const firstCharacterSlug = urlSlug(firstCostume.character.name)
+      const secondCharacterSlug = urlSlug(secondCostume.character.name)
 
       const [
         firstCostumeWeaponData, secondCostumeWeaponData, firstCostumeDebrisData, secondCostumeDebrisData
       ] = await Promise.all([
-        api.get(`/costume/weapon/${firstItem.costume_id}`),
-        api.get(`/costume/weapon/${secondItem.costume_id}`),
-        api.get(`/costume/debris/${firstItem.costume_id}`),
-        api.get(`/costume/debris/${secondItem.costume_id}`),
+        api.get(`/costume/weapon/${firstCostume.costume_id}`),
+        api.get(`/costume/weapon/${secondCostume.costume_id}`),
+        api.get(`/costume/debris/${firstCostume.costume_id}`),
+        api.get(`/costume/debris/${secondCostume.costume_id}`),
       ])
 
       const firstItemWeapon: ApiWeapon = firstCostumeWeaponData?.data
@@ -188,14 +218,14 @@ export default class Costume implements BaseDiscordCommand {
       const firstItemDebris: debris = firstCostumeDebrisData?.data
       const secondItemDebris: debris = secondCostumeDebrisData?.data
 
-      const description = await this.getItemDescriptionCostume(firstItem, secondItem)
+      const description = this.getItemDescriptionCostume(firstCostume, secondCostume)
 
-      const firstAwakeningAbility = firstItem.costume_ability_link[2]
-      const secondAwakeningAbility = secondItem.costume_ability_link[2]
+      const firstAwakeningAbility = firstCostume.costume_ability_link[2]
+      const secondAwakeningAbility = secondCostume.costume_ability_link[2]
 
       const embed = new EmbedBuilder()
         .setTitle(`
-          ${emojis[firstCharacterSlug]} ${firstItem.title} - ${secondItem.title} ${emojis[secondCharacterSlug]}
+          ${emojis[firstCharacterSlug]} ${firstCostume.title} - ${secondCostume.title} ${emojis[secondCharacterSlug]}
         `.trim())
         .setDescription(description)
         .setColor(Colors.Blurple)
@@ -203,14 +233,14 @@ export default class Costume implements BaseDiscordCommand {
           // First item ability
           {
             name: `${emojis[firstCharacterSlug]} Abilities`,
-            value: `\n${[...firstItem.costume_ability_link].splice(0, 2).map((ability) =>
+            value: `\n${[...firstCostume.costume_ability_link].splice(0, 2).map((ability) =>
               `${emojis.ability} [**${ability.costume_ability.name}**](https://nierrein.guide/ability/costume/${urlSlug(ability.costume_ability.name)}-${ability.costume_ability.ability_id})\n${ability.costume_ability.description}`).join('\n')}`,
               inline: true,
           },
           // Second item ability
           {
             name: `Abilities ${emojis[secondCharacterSlug]}`,
-            value: `\n${[...secondItem.costume_ability_link].splice(0, 2).map((ability) =>
+            value: `\n${[...secondCostume.costume_ability_link].splice(0, 2).map((ability) =>
               `${emojis.ability} [**${ability.costume_ability.name}**](https://nierrein.guide/ability/costume/${urlSlug(ability.costume_ability.name)}-${ability.costume_ability.ability_id})\n${ability.costume_ability.description}`).join('\n')}`,
               inline: true,
           },
@@ -248,12 +278,12 @@ export default class Costume implements BaseDiscordCommand {
           },
           {
             name: `${emojis[firstCharacterSlug]} Weapon`,
-            value: `${emojis[firstItem.weapon_type]}${emojis[firstItemWeapon.attribute]} [${firstItemWeapon.name}](https://nierrein.guide/weapons/${firstItemWeapon.slug})`,
+            value: `${emojis[firstCostume.weapon_type]}${emojis[firstItemWeapon.attribute]} [${firstItemWeapon.name}](https://nierrein.guide/weapons/${firstItemWeapon.slug})`,
             inline: true,
           },
           {
             name: `Weapon ${emojis[secondCharacterSlug]}`,
-            value: `${emojis[secondItem.weapon_type]}${emojis[secondItemWeapon.attribute]} [${secondItemWeapon.name}](https://nierrein.guide/weapons/${secondItemWeapon.slug})`,
+            value: `${emojis[secondCostume.weapon_type]}${emojis[secondItemWeapon.attribute]} [${secondItemWeapon.name}](https://nierrein.guide/weapons/${secondItemWeapon.slug})`,
             inline: true,
           },
         ])
@@ -265,5 +295,59 @@ export default class Costume implements BaseDiscordCommand {
       return
     }
 
+    // @ts-expect-error aa
+    const firstWeapon: ApiWeapon = firstItem as unknown
+    // @ts-expect-error aa
+    const secondWeapon: ApiWeapon  = secondItem as unknown
+
+    const description = this.getItemDescriptionWeapon(firstWeapon, secondWeapon)
+
+    const [firstWeaponCostumeData, secondWeaponCostumeData] = await Promise.all([
+      api.get(`/weapon/costume/${firstWeapon.weapon_id}`),
+      api.get(`/weapon/costume/${secondWeapon.weapon_id}`),
+    ])
+    const firstWeaponCostume: ApiCostume = firstWeaponCostumeData?.data
+    const secondWeaponCostume: ApiCostume = secondWeaponCostumeData?.data
+
+    const embed = new EmbedBuilder()
+      .setTitle(`
+        ${emojis[firstWeapon.weapon_type]}${emojis[firstWeapon.attribute]} ${firstWeapon.name} - ${secondWeapon.name} ${emojis[firstWeapon.attribute]}${emojis[secondWeapon.weapon_type]}
+      `.trim())
+      .setDescription(description)
+      .setColor(Colors.Blurple)
+      .addFields([
+        // First item ability
+        {
+          name: `${emojis[firstWeapon.weapon_type]}${emojis[firstWeapon.attribute]} Abilities`,
+          value: `\n${[...firstWeapon.weapon_ability_link].map((ability) =>
+            `${emojis.ability} [**${ability.weapon_ability.name}**](https://nierrein.guide/ability/weapon/${urlSlug(ability.weapon_ability.name)}-${ability.weapon_ability.ability_id})\n${ability.weapon_ability.description}`).join('\n')}`,
+            inline: true,
+        },
+        // Second item ability
+        {
+          name: `Abilities ${emojis[secondWeapon.weapon_type]}${emojis[secondWeapon.attribute]}`,
+          value: `\n${[...secondWeapon.weapon_ability_link].map((ability) =>
+            `${emojis.ability} [**${ability.weapon_ability.name}**](https://nierrein.guide/ability/weapon/${urlSlug(ability.weapon_ability.name)}-${ability.weapon_ability.ability_id})\n${ability.weapon_ability.description}`).join('\n')}`,
+            inline: true,
+        },
+        {
+          name: `Costumes`,
+          value: '-',
+        },
+        {
+          name: `${emojis[firstWeapon.weapon_type]}${emojis[firstWeapon.attribute]} Costume`,
+          value: `${emojis[urlSlug(firstWeaponCostume.character.name)]} [${firstWeaponCostume.character.name} - ${firstWeaponCostume.title}](https://nierrein.guide/characters/${urlSlug(firstWeaponCostume.character.name)}/${firstWeaponCostume.slug})`,
+          inline: true,
+        },
+        {
+          name: `Costume ${emojis[firstWeapon.attribute]}${emojis[firstWeapon.weapon_type]}`,
+          value: `${emojis[urlSlug(secondWeaponCostume.character.name)]} [${secondWeaponCostume.character.name} - ${secondWeaponCostume.title}](https://nierrein.guide/characters/${urlSlug(secondWeaponCostume.character.name)}/${secondWeaponCostume.slug})`,
+          inline: true,
+        },
+      ])
+
+    interaction.reply({
+      embeds: [embed]
+    })
   }
 }
